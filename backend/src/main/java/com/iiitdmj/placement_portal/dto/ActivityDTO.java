13c224b8960faefd5e589bcd54c25e774512ce6d
Prajwal Rayal <RAYALPRAJWAL82991@GMAIL.COM>
2024-09-06 01:09:35 +0530

    added DTO for activity

--- backend/src/main/java/com/iiitdmj/placement_portal/dto/ActivityDTO.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/dto/ActivityDTO.java
@@ -1,4 +1,22 @@
 package com.iiitdmj.placement_portal.dto;
 
+import com.iiitdmj.placement_portal.entity.Company;
+import com.iiitdmj.placement_portal.entity.Status;
+import com.iiitdmj.placement_portal.entity.User;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ActivityDTO {
+    @NotNull
+    private String description;
+    @NotNull
+    private Status status;
 }
+
+
