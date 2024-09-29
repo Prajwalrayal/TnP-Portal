8f82dfec17961054c1bec643b8ea0bd426853e20
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-09-29 22:12:48 +0530

    Updated Client and added the BL

--- backend/src/main/java/com/iiitdmj/placement_portal/service/ClientServiceImplementation.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/service/ClientServiceImplementation.java
@@ -1,7 +1,55 @@
 package com.iiitdmj.placement_portal.service;
 
+import com.iiitdmj.placement_portal.entity.Client;
+import com.iiitdmj.placement_portal.repository.ClientRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientServiceImplementation implements ClientService {
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Override
+    public List<Client> getAllClients() {
+        return clientRepository.findAll();
+    }
+
+    @Override
+    public Client getClientById(Integer id) {
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Client not found with id: " + id));
+    }
+
+    @Override
+    public Client addClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Override
+    public Client updateClient(Client client, Integer id) {
+        Client existingClient = clientRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Client not found with id: " + id));
+
+        existingClient.setEmail(client.getEmail());
+        existingClient.setFirstName(client.getFirstName());
+        existingClient.setLastName(client.getLastName());
+        existingClient.setDescription(client.getDescription());
+        existingClient.setLinkedinUrl(client.getLinkedinUrl());
+        existingClient.setCompany(client.getCompany());
+        existingClient.setPosition(client.getPosition());
+        existingClient.setMobileNumbers(client.getMobileNumbers());
+
+        return clientRepository.save(existingClient);
+    }
+
+    @Override
+    public void deleteClient(Integer id) {
+        Client existingClient = clientRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Client not found with id: " + id));
+        clientRepository.delete(existingClient);
+    }
 }
