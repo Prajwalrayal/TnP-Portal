f6751fb4202985816a481321ecc7a572b47bf2b4
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-09-29 22:10:17 +0530

    Updated Activity Services implementation for handling logs and others

--- backend/src/main/java/com/iiitdmj/placement_portal/service/ActivityService.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/service/ActivityService.java
@@ -1,16 +1,23 @@
 package com.iiitdmj.placement_portal.service;
 
 import com.iiitdmj.placement_portal.dto.ActivityDTO;
+import com.iiitdmj.placement_portal.dto.ActivityLogDTO;
+import com.iiitdmj.placement_portal.dto.ActivityResponse;
 import com.iiitdmj.placement_portal.entity.Activity;
+import com.iiitdmj.placement_portal.entity.ActivityLog;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public interface ActivityService {
-    Activity addActivity(Activity activity);
+    ActivityResponse addActivity(ActivityDTO activity);
 
-    List<Activity> getAllActivites();
+    List<ActivityResponse> getAllActivites();
 
     Activity updateActivity(ActivityDTO updateActivity, Integer id);
+
+    ActivityLog addLog(ActivityLogDTO activityLogDTO);
+
+    List<ActivityLog> getLogs(Integer id);
 }
--- backend/src/main/java/com/iiitdmj/placement_portal/service/ActivityServiceImplementation.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/service/ActivityServiceImplementation.java
@@ -1,8 +1,15 @@
 package com.iiitdmj.placement_portal.service;
 
 import com.iiitdmj.placement_portal.dto.ActivityDTO;
+import com.iiitdmj.placement_portal.dto.ActivityLogDTO;
+import com.iiitdmj.placement_portal.dto.ActivityResponse;
 import com.iiitdmj.placement_portal.entity.Activity;
+import com.iiitdmj.placement_portal.entity.ActivityLog;
+import com.iiitdmj.placement_portal.entity.User;
+import com.iiitdmj.placement_portal.repository.ActivityLogRepository;
 import com.iiitdmj.placement_portal.repository.ActivityRepository;
+import com.iiitdmj.placement_portal.repository.CompanyRepository;
+import com.iiitdmj.placement_portal.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,20 +22,68 @@ public class ActivityServiceImplementation implements ActivityService {
     @Autowired
     private final ActivityRepository activityRepository;
 
+    @Autowired
+    private  CompanyRepository companyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ActivityLogRepository activityLogRepository;
+
     public ActivityServiceImplementation(ActivityRepository activityRepository) {
         this.activityRepository = activityRepository;
     }
 
     @Override
-    public Activity addActivity(Activity activity) {
-        activity.setCreatedAt(LocalDateTime.now());
-        activity.setLastUpdated(LocalDateTime.now());
-        return activityRepository.save(activity);
+    public List<ActivityLog> getLogs(Integer id) {
+        return activityLogRepository.findByActivityId(id);
     }
 
     @Override
-    public List<Activity> getAllActivites() {
-        return activityRepository.findAll();
+    public ActivityLog addLog(ActivityLogDTO activityLogDTO) {
+        Activity activity = activityRepository.findById(activityLogDTO.getActivityId())
+                .orElseThrow(() -> new RuntimeException("Activity not found"));
+
+        ActivityLog activityLog = new ActivityLog();
+        activityLog.setLog(activityLogDTO.getLog());
+        activityLog.setTimestamp(LocalDateTime.now());
+        activityLog.setActivity(activity);
+
+        return activityLogRepository.save(activityLog);
+    }
+
+    @Override
+    public ActivityResponse addActivity(ActivityDTO activityDTO) {
+        Activity activity = new Activity();
+        activity.setStatus(activityDTO.getStatus());
+        activity.setDescription(activityDTO.getDescription());
+        activity.setCompany(companyRepository.findById(activityDTO
+                .getCompanyId())
+                .orElseThrow(() -> new EntityNotFoundException("Company not found with id: " + activityDTO.getCompanyId())));
+        activity.setUser(userRepository.findByEmail(activityDTO.getUserEmail()));
+        activity.setCreatedAt(LocalDateTime.now());
+        activity.setLastUpdated(LocalDateTime.now());
+        activityRepository.save(activity);
+
+        ActivityResponse activityResponse = new ActivityResponse(
+                activity.getId(),
+                activity.getDescription(),
+                activity.getStatus(),
+                activity.getCompany(),
+                activity.getUser().getEmail(),
+                activity.getUser().getFirstName(),
+                activity.getCreatedAt(),
+                activity.getLastUpdated()
+        );
+
+        return activityResponse;
+    }
+
+    @Override
+    public List<ActivityResponse> getAllActivites() {
+        List<Activity> activities = activityRepository.findAll();
+        return ActivityResponse.fromActivityList(activities);
     }
 
     @Override
@@ -38,6 +93,18 @@ public class ActivityServiceImplementation implements ActivityService {
         currentActivity.setLastUpdated(LocalDateTime.now());
         currentActivity.setStatus(updateActivity.getStatus());
         currentActivity.setDescription(updateActivity.getDescription());
+        // if companyId and userId exists
+        if (updateActivity.getCompanyId() != null ) {
+            currentActivity.setCompany(companyRepository.findById(updateActivity.getCompanyId())
+                    .orElseThrow(() -> new EntityNotFoundException("Company not found with id: " + updateActivity.getCompanyId())));
+        }
+        if (updateActivity.getUserEmail() != null) {
+            User user = userRepository.findByEmail(updateActivity.getUserEmail());
+            if (user == null) {
+                throw new EntityNotFoundException("User not found with email: " + updateActivity.getUserEmail());
+            }
+            currentActivity.setUser(user);
+        }
 
         return activityRepository.save(currentActivity) ;
     }
