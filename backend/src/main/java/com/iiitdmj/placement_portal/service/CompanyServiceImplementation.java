c65a9941d58b994d040143dd3d22c6ee41b477e4
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-09-29 22:14:23 +0530

    Added position and salary logic and structs

--- backend/src/main/java/com/iiitdmj/placement_portal/service/CompanyServiceImplementation.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/service/CompanyServiceImplementation.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 public class CompanyServiceImplementation implements CompanyService {
   @Autowired
-  private CompanyRepository companyRepository;
+  private final CompanyRepository companyRepository;
 
   CompanyServiceImplementation(CompanyRepository companyRepository) {
     this.companyRepository = companyRepository;
@@ -45,6 +45,9 @@ public class CompanyServiceImplementation implements CompanyService {
     currentCompany.setDescription(company.getDescription());
     currentCompany.setWebsite(company.getWebsite());
 
+    // setSalary and setRolesOffered methods are not present in Company class
+    currentCompany.setSalaries(company.getSalaries());
+    currentCompany.setRolesOffered(company.getRolesOffered());
     return companyRepository.save(currentCompany);
   }
 }
