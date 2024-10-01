73035b8e9cd778c2db25ccd3c4368bebc9fca7d4
Priyansh61 <21bec080@iiitdmj.ac.in>
2024-10-01 20:31:16 +0530

    constructor added

--- backend/src/main/java/com/iiitdmj/placement_portal/entity/AdminRole.java
+++ backend/src/main/java/com/iiitdmj/placement_portal/entity/AdminRole.java
@@ -1,7 +1,19 @@
 package com.iiitdmj.placement_portal.entity;
 
+import com.iiitdmj.placement_portal.constants.UserRole;
 import jakarta.persistence.Entity;
 
 @Entity
 public class AdminRole extends User {
+    public AdminRole() {}
+    public AdminRole(User user) {
+        this.setEmail(user.getEmail());
+        this.setFirstName(user.getFirstName());
+        this.setLastName(user.getLastName());
+        this.setPassword(user.getPassword());
+        this.setRole(UserRole.ADMIN);
+        this.setLinkedinUrl(user.getLinkedinUrl());
+        this.setCreatedAt(user.getCreatedAt());
+        this.setLastLoginAt(user.getLastLoginAt());
+    }
 }
